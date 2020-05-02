6aa7861e66f0c92b47714673f8ccd93a31d6c321
songmeo <shuong1012@gmail.com>
2020-05-02 15:15:40 +0300

    done

--- hw4/scrape.py
+++ hw4/scrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import re, jq
 
 url = 'https://www.osta.ee/en/category/computers'
 data = {}
@@ -10,18 +11,21 @@ page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 pages = int(soup.find("span", {"class": "page-count"}).text)
 
-#all product titles
+#all product
 for i in range(pages + 1):
 	tmp = url + '/page-' + str(i)
 	page = requests.get(tmp)
 	soup = BeautifulSoup(page.text, 'html.parser')
-	offers = soup.find_all('p[offer-thumb]')
+	offers = soup.find_all("figure", {"class":"offer-thumb"})
 	for i in offers:
-		children = i.findChildren('p', recursive=True)
-		for child in children:
-			print(child.text)
-
-with open('data.txt', 'w') as outfile:
+		image = i.find("figure", {"class":"offer-thumb__image"}).find("a")["data-original"]
+		title = i.find("p", {"class":"offer-thumb__title"}).text
+		price = i.find(lambda tag:tag.name=="span" and "€" in tag.text).text
+		data['items'].append({
+			'title': title,
+			'price': price,
+			'image': image
+		})
+	
+with open('data.json', 'w') as outfile:
 	json.dump(data, outfile)
-	#print(tmp)
-#print(soup.prettify())

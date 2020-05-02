18f40e3042d0be8686bc92f7b93b5c7fb11c4bc9
songmeo <shuong1012@gmail.com>
2020-05-02 13:19:57 +0300

    find children

--- hw4/scrape.py
+++ hw4/scrape.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = 'https://www.osta.ee/en/category/computers'
-
+data = {}
+data['items'] = []
 #find total pages
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -13,7 +15,13 @@ for i in range(pages + 1):
 	tmp = url + '/page-' + str(i)
 	page = requests.get(tmp)
 	soup = BeautifulSoup(page.text, 'html.parser')
-	for i in soup.select('a[data-view-src]'):
-		print(i.text)
+	offers = soup.find_all('p[offer-thumb]')
+	for i in offers:
+		children = i.findChildren('p', recursive=True)
+		for child in children:
+			print(child.text)
+
+with open('data.txt', 'w') as outfile:
+	json.dump(data, outfile)
 	#print(tmp)
 #print(soup.prettify())
